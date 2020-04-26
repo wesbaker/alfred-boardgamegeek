c15577329da52c1f67b5bdc3f49301869688edd2
wesbaker <wes@wesbaker.com>
2020-04-26 11:16:05 -0400

    Update circle config, fix prettier issues

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ alfy
   .fetch("https://boardgamegeek.com/search/boardgame", {
     query: {
       showcount: 20,
-      q: alfy.input
-    }
+      q: alfy.input,
+    },
   })
   .then(({ items }) =>
     alfy.output(
       items.map(({ name, href, yearpublished }) => ({
         title: name,
         subtitle: `Published in ${yearpublished}`,
-        arg: `https://boardgamegeek.com${href}`
+        arg: `https://boardgamegeek.com${href}`,
       }))
     )
   );
